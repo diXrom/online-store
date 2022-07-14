0705b8ff51ba1579dbb16273f729c3bdecf3e64c
Tolegen Timur <demigodkz@gmail.com>
2022-07-15 04:38:57 +0600

    refactor: add container for Footer and Header components

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,22 @@
-import { Toolbar, Typography, Link } from '@mui/material';
+import { Toolbar, Typography, Link, Container } from '@mui/material';
+import { memo } from 'react';
 
 const Footer = () => (
-  <Toolbar sx={{ mt: 2, pt: 1, pb: 1, background: '#000000'}}>
-    <Link href='https://rs.school/' target='_blank' rel='noopener' underline='none'>
-      <img src='./images/Logo RSS.svg' loading='lazy' alt='' />
-    </Link>
-    <Typography variant='h6' component='div' sx={{ flexGrow: 1, ml: 2,color:'#F3E600' }}>
-      2022
-    </Typography>
-    <Link href='https://github.com/diXrom' target='_blank' rel='noopener' underline='none'>
-      <Typography variant='h6' component='div' sx={{color:'#F3E600' }}>
-        GitHub
+  <Container maxWidth='xl'>
+    <Toolbar sx={{ mt: 2, pt: 1, pb: 1, background: '#000000', borderRadius: 1 }}>
+      <Link href='https://rs.school/' target='_blank' rel='noopener' underline='none'>
+        <img src='./images/Logo RSS.svg' loading='lazy' alt='' />
+      </Link>
+      <Typography variant='h6' component='div' sx={{ flexGrow: 1, ml: 2, color: '#F3E600' }}>
+        2022
       </Typography>
-    </Link>
-  </Toolbar>
+      <Link href='https://github.com/diXrom' target='_blank' rel='noopener' underline='none'>
+        <Typography variant='h6' component='div' sx={{ color: '#F3E600' }}>
+          GitHub
+        </Typography>
+      </Link>
+    </Toolbar>
+  </Container>
 );
 
-export default Footer;
+export default memo(Footer);
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
-import { FC } from 'react';
+import { FC, memo } from 'react';
 
-import { AppBar, Toolbar, Typography, IconButton, Badge } from '@mui/material';
+import { AppBar, Toolbar, Typography, IconButton, Badge, Container } from '@mui/material';
 import { ShoppingCartOutlined, Store } from '@mui/icons-material';
 import { IBasket } from '../types';
 
@@ -8,28 +8,29 @@ interface IHeader {
   basket: IBasket[];
 }
 const Header: FC<IHeader> = ({ basket }) => {
-  console.log(basket)
   return (
-    <AppBar position='static'>
-      <Toolbar sx={{ paddingTop: 1, paddingBottom: 1 }}>
-        <IconButton edge='start' color='info'>
-          <Store fontSize='large' />
-        </IconButton>
-        <Typography variant='h5' component='h3' sx={{ flexGrow: 1 }}>
-          Online Store
-        </Typography>
-        <Badge
-          color='info'
-          badgeContent={basket.reduce((acc, item) => item.amount + acc, 0)}
-          overlap='circular'
-        >
-          <IconButton edge='start' color='inherit'>
-            <ShoppingCartOutlined fontSize='large' />
+    <Container maxWidth='xl'>
+      <AppBar position='static' sx={{borderRadius: 1 }}>
+        <Toolbar sx={{ paddingTop: 1, paddingBottom: 1,borderRadius: 2 }}>
+          <IconButton edge='start' color='info'>
+            <Store fontSize='large' />
           </IconButton>
-        </Badge>
-      </Toolbar>
-    </AppBar>
+          <Typography variant='h5' component='h3' sx={{ flexGrow: 1 }}>
+            Online Store
+          </Typography>
+          <Badge
+            color='info'
+            badgeContent={basket.reduce((acc, item) => item.amount + acc, 0)}
+            overlap='circular'
+          >
+            <IconButton edge='start' color='inherit'>
+              <ShoppingCartOutlined fontSize='large' />
+            </IconButton>
+          </Badge>
+        </Toolbar>
+      </AppBar>
+    </Container>
   );
 };
 
-export default Header;
+export default memo(Header);
